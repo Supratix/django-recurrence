b31e6654ccae4ae8f95af22ed8a714e67f105bbb
Zac Littleberry <zaclittleberry@gmail.com>
2018-11-18 12:22:54 -0500

    Update recurrence-widget.js
    change pgettext to gettext

--- recurrence/static/recurrence/js/recurrence-widget.js
+++ recurrence/static/recurrence/js/recurrence-widget.js
@@ -1603,7 +1603,7 @@ recurrence.widget.DateForm.prototype = {
     },
 
     get_display_text: function() {
-        var text = recurrence.date.format(this.date, pgettext('date', '%l, %F %j, %Y'));
+        var text = recurrence.date.format(this.date, gettext('date', '%l, %F %j, %Y'));
         if (this.mode == recurrence.widget.EXCLUSION)
             text = recurrence.display.mode.exclusion + ' ' + text;
         return recurrence.string.capitalize(text);
